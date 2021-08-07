042020339785e03ccb45677c2b4e3b416d664d4f
Santiago Rivera R <sriverarestrepo@gmail.com>
2021-08-06 20:37:10 -0500

    Inicializacion de proyecto demo

---
+++ src/main/java/com/srr/srdemo/SrdemoApplication.java
@@ -0,0 +1,13 @@
+package com.srr.srdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srr/srdemo/SrdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srr.srdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
